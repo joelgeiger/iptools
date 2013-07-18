1b72498e2dce2c5efd4beb6db38e42d57c69a9b5
Joel Geiger <joel@sibilantsolutions.com>
2013-07-18 11:44:38 -0400

    Rename IrcReceiver -> LineParserBuffer and implement it as a decorator.

---
+++ src/main/java/com/sibilantsolutions/iptools/util/LineParserBuffer.java
@@ -0,0 +1,154 @@
+package com.sibilantsolutions.iptools.util;
+
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sibilantsolutions.iptools.event.ReceiveEvt;
+import com.sibilantsolutions.iptools.event.SocketListenerI;
+
+public class LineParserBuffer implements SocketListenerI
+{
+    final static private Logger log = LoggerFactory.getLogger( LineParserBuffer.class );
+
+    private SocketListenerI receiver;
+
+    //private ByteBuffer buf = ByteBuffer.allocate( 2048 );
+        //TODO: This should be switched back to something smaller.  The real solution ought to see
+        //how much room is left in the buffer and only put in that many bytes from the
+        //ReceiveEvt, and check to see if there is a full line yet.  If so, great.  If not then
+        //it means we exceeded our max line length without finding a line.  So I guess the size
+        //here ought to be no bigger than the max allowed line (is it defined in the RFC?).
+    private ByteBuffer buf = ByteBuffer.allocate( 1024 * 8 );
+
+/*
+    private void doReceive( ReceiveEvt evt )
+    {
+        //TODO: Handle partial receives.
+        ByteArrayInputStream bis = new ByteArrayInputStream( evt.getData(), evt.getOffset(), evt.getLength() );
+        InputStreamReader isr = new InputStreamReader( bis );
+        BufferedReader reader = new BufferedReader( isr );
+
+        String line;
+
+        try
+        {
+            while ( ( line = reader.readLine() ) != null )
+            {
+                log.info( "The line={}", line );
+                ircDataProc.onReceiveLine( line );
+            }
+        }
+        catch ( IOException e )
+        {
+            // TODO Auto-generated catch block
+            throw new UnsupportedOperationException( "OGTE TODO!", e );
+        }
+    }
+*/
+
+    private void doReceiveBuffer( ReceiveEvt evt )
+    {
+        log.trace( "BEFORE PUT: The buf={}", buf );
+
+        buf.put( evt.getData(), evt.getOffset(), evt.getLength() );
+
+        log.trace( "AFTER PUT: The buf={}", buf );
+
+        buf.flip();
+
+        log.trace( "AFTER FLIP: The buf={}", buf );
+
+        boolean foundCrLf = false;
+
+        while ( buf.hasRemaining() )
+        {
+            foundCrLf = false;
+
+            boolean foundCr = false;
+
+                //TODO: Don't collect the bytes as we go, rather find the end index and get the
+                //the whole string at once.  Will be more efficient than growing the stringbuilder.
+            StringBuilder sBuf = new StringBuilder();
+
+            log.trace( "BEFORE MARK: The buf={}", buf );
+            buf.mark();
+            log.trace( "AFTER MARK: The buf={}", buf );
+
+            while ( ! foundCrLf && buf.hasRemaining() )
+            {
+                byte b = buf.get();
+
+                if ( b == '\r' )
+                {
+                    foundCr = ! foundCr;
+                }
+                else if ( b == '\n' )
+                {
+                    if ( foundCr )
+                    {
+                        foundCrLf = true;
+                    }
+                    else
+                        foundCr = false;
+                }
+                else
+                {
+                    foundCr = false;
+                }
+
+                    //This is intended to allow stray CRs or LFs to be added to the line.
+                    //Can it ever happen?
+                if ( ! foundCr )
+                    sBuf.append( (char)b );   //TODO Is this the right way to convert >127 bytes?
+            }
+
+            if ( foundCrLf )
+            {
+                log.trace( "FOUND CRLF: The buf={}", buf );
+
+                String line = sBuf.toString();
+                log.debug( "The line={}", line );
+                //ircDataProc.onReceiveLine( line );
+                receiver.onReceive( new ReceiveEvt( line.getBytes(), evt.getSource() ) );
+            }
+        }
+
+        if ( foundCrLf )
+        {
+            log.trace( "BEFORE CLEAR: The buf={}", buf );
+            buf.clear();
+            log.trace( "AFTER CLEAR: The buf={}", buf );
+        }
+        else
+        {
+            log.trace( "NOT FOUND CRLF: The buf={}", buf );
+            buf.reset();
+            log.trace( "AFTER RESET: The buf={}", buf );
+            buf.compact();
+            log.trace( "AFTER COMPACT: The buf={}", buf );
+            //throw new UnsupportedOperationException( "TODO" );
+        }
+
+        log.trace( "ALL DONE: The buf={}", buf );
+    }
+
+    @Override
+    public void onReceive( ReceiveEvt evt )
+    {
+        //doReceive( evt );
+        doReceiveBuffer( evt );
+    }
+
+    public SocketListenerI getReceiver()
+    {
+        return receiver;
+    }
+
+    public void setReceiver( SocketListenerI receiver )
+    {
+        this.receiver = receiver;
+    }
+
+}
---
+++ src/test/java/com/sibilantsolutions/iptools/util/LineParserBufferTest.java
@@ -0,0 +1,163 @@
+package com.sibilantsolutions.iptools.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.sibilantsolutions.iptools.event.ReceiveEvt;
+import com.sibilantsolutions.iptools.event.SocketListenerI;
+
+public class LineParserBufferTest
+{
+
+    @Test
+    public void testOnReceive()
+    {
+        String data = "abc\r\n";
+        ReceiveEvt evt = new ReceiveEvt( data.getBytes(), null );
+
+        MyProc ircDataProc = new MyProc();
+
+        LineParserBuffer ircReceiver = new LineParserBuffer();
+
+        ircReceiver.setReceiver( ircDataProc );
+
+        ircReceiver.onReceive( evt );
+
+        assertEquals( 1, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+    }
+
+    @Test
+    public void testOnReceive_multi()
+    {
+        String data = "abc\r\ndef\r\n";
+        ReceiveEvt evt = new ReceiveEvt( data.getBytes(), null );
+
+        MyProc ircDataProc = new MyProc();
+
+        LineParserBuffer ircReceiver = new LineParserBuffer();
+
+        ircReceiver.setReceiver( ircDataProc );
+
+        ircReceiver.onReceive( evt );
+
+        assertEquals( 2, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+        assertEquals( "def", ircDataProc.lines.get( 1 ) );
+    }
+
+    @Test
+    public void testOnReceive_partial()
+    {
+        String data = "ab";
+        ReceiveEvt evt = new ReceiveEvt( data.getBytes(), null );
+
+        MyProc ircDataProc = new MyProc();
+
+        LineParserBuffer ircReceiver = new LineParserBuffer();
+
+        ircReceiver.setReceiver( ircDataProc );
+
+        ircReceiver.onReceive( evt );
+
+        assertEquals( 0, ircDataProc.lines.size() );
+    }
+
+    @Test
+    public void testOnReceive_multiAndPartial()
+    {
+        String data = "abc\r\ndef\r\ngh";
+        ReceiveEvt evt = new ReceiveEvt( data.getBytes(), null );
+
+        MyProc ircDataProc = new MyProc();
+
+        LineParserBuffer ircReceiver = new LineParserBuffer();
+
+        ircReceiver.setReceiver( ircDataProc );
+
+        ircReceiver.onReceive( evt );
+
+        assertEquals( 2, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+        assertEquals( "def", ircDataProc.lines.get( 1 ) );
+
+        evt = new ReceiveEvt( "i\r\n".getBytes(), null );
+
+        ircReceiver.onReceive( evt );
+
+        assertEquals( 3, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+        assertEquals( "def", ircDataProc.lines.get( 1 ) );
+        assertEquals( "ghi", ircDataProc.lines.get( 2 ) );
+    }
+
+    @Test
+    public void testOnReceive_singleBytes()
+    {
+        MyProc ircDataProc = new MyProc();
+
+        LineParserBuffer ircReceiver = new LineParserBuffer();
+
+        ircReceiver.setReceiver( ircDataProc );
+
+        ircReceiver.onReceive( new ReceiveEvt( "a".getBytes(), null ) );
+        assertEquals( 0, ircDataProc.lines.size() );
+
+        ircReceiver.onReceive( new ReceiveEvt( "b".getBytes(), null ) );
+        assertEquals( 0, ircDataProc.lines.size() );
+
+        ircReceiver.onReceive( new ReceiveEvt( "c".getBytes(), null ) );
+        assertEquals( 0, ircDataProc.lines.size() );
+
+        ircReceiver.onReceive( new ReceiveEvt( "\r".getBytes(), null ) );
+        assertEquals( 0, ircDataProc.lines.size() );
+
+        ircReceiver.onReceive( new ReceiveEvt( "\n".getBytes(), null ) );
+        assertEquals( 1, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+
+
+        ircReceiver.onReceive( new ReceiveEvt( "d".getBytes(), null ) );
+        assertEquals( 1, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+
+        ircReceiver.onReceive( new ReceiveEvt( "e".getBytes(), null ) );
+        assertEquals( 1, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+
+
+        ircReceiver.onReceive( new ReceiveEvt( "f".getBytes(), null ) );
+        assertEquals( 1, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+
+
+        ircReceiver.onReceive( new ReceiveEvt( "\r".getBytes(), null ) );
+        assertEquals( 1, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+
+        ircReceiver.onReceive( new ReceiveEvt( "\n".getBytes(), null ) );
+        assertEquals( 2, ircDataProc.lines.size() );
+        assertEquals( "abc", ircDataProc.lines.get( 0 ) );
+        assertEquals( "def", ircDataProc.lines.get( 1 ) );
+
+    }
+
+    static private class MyProc implements SocketListenerI
+    {
+
+        private List<String> lines = new ArrayList<String>();
+
+        @Override
+        public void onReceive( ReceiveEvt evt )
+        {
+                //TODO: Make sure that this uses the correct encoding.
+            String line = new String( evt.getData(), evt.getOffset(), evt.getLength() );
+            lines.add( line );
+        }
+    }
+
+}
