8230d0b098c06fb70cc7584156489df2f53398f9
Joel Geiger <joel@sibilantsolutions.com>
2014-03-28 19:47:35 -0400

    Use @SuppressWarnings for restricted sun.security.* package.
    Works to suppress warnings in Eclipse, but did not affect
    Maven command line.

--- src/main/java/com/sibilantsolutions/iptools/util/CertDuplicator.java
+++ src/main/java/com/sibilantsolutions/iptools/util/CertDuplicator.java
@@ -28,6 +28,7 @@ import sun.security.x509.X500Name;
 import sun.security.x509.X509CertImpl;
 import sun.security.x509.X509CertInfo;
 
+@SuppressWarnings( "restriction" )
 abstract public class CertDuplicator
 {
 
