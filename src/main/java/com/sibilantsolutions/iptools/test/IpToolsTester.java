c111a0540d0466a6e8a092e66a668c685b7c90f0
Joel Geiger <joel@sibilantsolutions.com>
2014-07-24 17:19:56 -0400

    pom updates:
    - Remove unused dependencies & code
    - Bump version to 0.0.2-SNAPSHOT
    - Use latest slf4j & logback

--- src/main/java/com/sibilantsolutions/iptools/test/IpToolsTester.java
+++ src/main/java/com/sibilantsolutions/iptools/test/IpToolsTester.java
@@ -1,37 +1,21 @@
 package com.sibilantsolutions.iptools.test;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import javax.net.ServerSocketFactory;
-
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.beust.jcommander.JCommander;
-import com.sibilantsolutions.iptools.cli.CommandAggregator;
-import com.sibilantsolutions.iptools.cli.CommandHttp;
-import com.sibilantsolutions.iptools.cli.CommandIrc;
-import com.sibilantsolutions.iptools.cli.CommandRedir;
-import com.sibilantsolutions.iptools.cli.CommandSocketTwoPane;
-import com.sibilantsolutions.iptools.event.ConnectEvent;
-import com.sibilantsolutions.iptools.event.ConnectionListenerI;
 import com.sibilantsolutions.iptools.gui.SocketTwoPane;
-import com.sibilantsolutions.iptools.redir.Redirector;
 import com.sibilantsolutions.iptools.util.DurationLoggingRunnable;
 
 public class IpToolsTester
 {
     final static private Logger log = LoggerFactory.getLogger( IpToolsTester.class );
 
-    static private String[] args;
+//    static private String[] args;
 
     static public void main( final String[] args )
     {
+        log.info( "main() started." );
+
         try
         {
             Runnable r = new Runnable() {
@@ -39,9 +23,7 @@ public class IpToolsTester
                 @Override
                 public void run()
                 {
-                    log.info( "main() started." );
-
-                    IpToolsTester.args = args;
+//                    IpToolsTester.args = args;
 
                     new SocketTwoPane().buildUi();
                     //new IpToolsTester().test();
@@ -60,62 +42,7 @@ public class IpToolsTester
             log.error( "Trouble:", new RuntimeException( e ) );
         }
     }
-
-    private void args4jTest( String[] args )
-    {
-        CommandAggregator bean = new CommandAggregator();
-        CmdLineParser parser = new CmdLineParser( bean );
-        try
-        {
-            log.info( "Printing usage." );
-            parser.printUsage( System.out );
-            log.info( "Done printing usage." );
-            parser.parseArgument( args );
-            log.info( "Printing usage." );
-            parser.printSingleLineUsage( System.out );
-            log.info( "Done printing usage." );
-            log.info( "cmd={}", bean.cmd );
-            CommandIrc irc = (CommandIrc)bean.cmd;
-            log.info( "file={}", irc.getFilename() );
-        }
-        catch ( CmdLineException e )
-        {
-            // TODO Auto-generated catch block
-            throw new UnsupportedOperationException( "OGTE TODO!", e );
-        }
-    }
-
-    private void jCommanderTest( String[] args )
-    {
-        JCommander jc = new JCommander();
-
-        jc.addCommand( CommandIrc.COMMAND_NAME, CommandIrc.INSTANCE, CommandIrc.ALIASES );
-        jc.addCommand( CommandRedir.COMMAND_NAME, CommandRedir.INSTANCE, CommandRedir.ALIASES );
-        jc.addCommand( CommandSocketTwoPane.COMMAND_NAME, CommandSocketTwoPane.INSTANCE, CommandSocketTwoPane.ALIASES );
-        jc.addCommand( CommandHttp.COMMAND_NAME, CommandHttp.INSTANCE, CommandHttp.ALIASES );
-
-        jc.usage();
-
-        jc.parse( args );
-
-        String parsedCommand = jc.getParsedCommand();
-
-        switch ( parsedCommand )
-        {
-            case CommandIrc.COMMAND_NAME:
-                String filename = CommandIrc.INSTANCE.getFilename();
-                log.info( "IRC filename={}.", filename );
-                break;
-
-            case CommandRedir.COMMAND_NAME:
-                log.info( "Redir." );
-                break;
-
-            default:
-                throw new RuntimeException( "OGTE TODO" );
-        }
-    }
-
+/* out
     private void test()
     {
         ServerSocketFactory ssf = ServerSocketFactory.getDefault();
@@ -166,5 +93,5 @@ public class IpToolsTester
 
         return redirector;
     }
-
+//*/
 }
