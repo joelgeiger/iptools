713eb5b953761fdb7cf910bc8b98e15824ad6446
Joel Geiger <joel@sibilantsolutions.com>
2014-08-27 16:34:43 -0400

    Attempt to process all remaining tasks in the queue during lost connection.

--- src/main/java/com/sibilantsolutions/iptools/net/ReceiveQueue.java
+++ src/main/java/com/sibilantsolutions/iptools/net/ReceiveQueue.java
@@ -1,6 +1,8 @@
 package com.sibilantsolutions.iptools.net;
 
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -57,8 +59,13 @@ public class ReceiveQueue implements SocketListenerI
     @Override
     public void onLostConnection( LostConnectionEvt evt )
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException( "MY TODO!" );
+        log.info( "Lost connection: Shutting down executor service." );
+
+        shutdownAndAwaitTermination( executorService, executorService.getQueue().size() );
+
+        log.info( "Lost connection: Firing event to destination." );
+
+        dest.onLostConnection( evt );
     }
 
     @Override
@@ -74,6 +81,57 @@ public class ReceiveQueue implements SocketListenerI
         };
 
         executorService.execute( r );
+    }
+
+        //Adapted from example in javadoc of ExecutorService.
+    private void shutdownAndAwaitTermination( ExecutorService pool, int queueSize )
+    {
+        log.info( "Shutting down executor service; queue size={}.", queueSize );
+
+        pool.shutdown(); // Disable new tasks from being submitted
+
+        log.info( "Waiting for executor service to finish all queued tasks." );
+
+        try
+        {
+            // Wait a while for existing tasks to terminate
+            if ( ! pool.awaitTermination( 60, TimeUnit.SECONDS ) )
+            {
+                log.info( "Executor service did not terminate before timeout; forcing shutdown." );
+
+                List<Runnable> queuedTasks = pool.shutdownNow(); // Cancel currently executing tasks
+
+                log.info( "Discarded {} tasks that were still queued; still waiting for executor service to terminate."
+                        , queuedTasks.size() );
+
+                // Wait a while for tasks to respond to being cancelled
+                if ( ! pool.awaitTermination( 60, TimeUnit.SECONDS ) )
+                {
+                    log.error( "Pool did not terminate." );
+                }
+                else
+                {
+                    log.info( "Executor service terminated after forced shutdown." );
+                }
+            }
+            else
+            {
+                log.info( "Executor service finished all queued tasks and terminated." );
+            }
+        }
+        catch ( InterruptedException ie )
+        {
+            log.error( "Interrupted while waiting for pool to terminate; forcing shutdown.", ie );
+
+            // (Re-)Cancel if current thread also interrupted
+            List<Runnable> queuedTasks = pool.shutdownNow();
+
+            log.info( "Discarded {} tasks that were still queued; no longer waiting for executor service to terminate."
+                    , queuedTasks.size() );
+
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
     }
 
 }
