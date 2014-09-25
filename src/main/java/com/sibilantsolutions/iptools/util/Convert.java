83756f2f6cbbee09f9dfef908a82b9dc22c787d1
Joel Geiger <joel@sibilantsolutions.com>
2014-09-25 15:59:14 -0400

    Add methods to work with retrieving bytes from ByteBuffer.

--- src/main/java/com/sibilantsolutions/iptools/util/Convert.java
+++ src/main/java/com/sibilantsolutions/iptools/util/Convert.java
@@ -1,11 +1,30 @@
 package com.sibilantsolutions.iptools.util;
 
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public abstract class Convert
 {
 
     private Convert() {}    //Prevent instantiation.
+
+    static public int byteToNum( byte b )
+    {
+        return b & 0xFF;
+    }
+
+    static public int getByte( ByteBuffer bb )
+    {
+        return byteToNum( bb.get() );
+    }
+
+    static public long toNum( ByteBuffer bb, int numBytes )
+    {
+        byte[] bytes = new byte[numBytes];
+        bb.get( bytes );
+
+        return toNum( bytes, 0, numBytes, bb.order() );
+    }
 
     static public long toNum( byte[] bytes, int offset, int length )
     {
